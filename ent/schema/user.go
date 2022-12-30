8a980124f5c65b774d66c7c845bf856ed453a481
SOK14 <yoshihiro_tsujii@yahoo.co.jp>
2022-12-30 15:31:42 +0900

    update edge of user

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
 )
 
@@ -25,5 +26,10 @@ func (User) Fields() []ent.Field {
 
 // Edges of the User.
 func (User) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("company", Company.Type).
+			Unique().
+			Field("company_id").
+			Required(),
+	}
 }
