a6bab19a000b6efe8e50a916b53ab5ccc77f83e5
SOK14 <yoshihiro_tsujii@yahoo.co.jp>
2022-12-30 15:44:51 +0900

    update main

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"demo_goent/ent/migrate"
 	"log"
+	"os"
 
 	"entgo.io/ent/examples/fs/ent"
 
@@ -15,9 +17,24 @@ func main() {
 		log.Fatalf("failed opening connection to mysql: %v", err)
 	}
 	defer client.Close()
-	// Run the auto migration tool.
-	if err := client.Schema.Create(context.Background()); err != nil {
+	ctx := context.Background()
+	// dump migration changes to stdout
+	if err := client.Schema.WriteTo(ctx, os.Stdout, migrate.WithForeignKeys(false)); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 	log.Print("ent sample done")
 }
+
+// -- DB接続確認
+// func main() {
+// 	client, err := ent.Open("mysql", "root:myrootpassword@tcp(localhost:3306)/mydb?parseTime=True")
+// 	if err != nil {
+// 		log.Fatalf("failed opening connection to mysql: %v", err)
+// 	}
+// 	defer client.Close()
+// 	// Run the auto migration tool.
+// 	if err := client.Schema.Create(context.Background()); err != nil {
+// 		log.Fatalf("failed creating schema resources: %v", err)
+// 	}
+// 	log.Print("ent sample done")
+// }
