cc82ef21cc04c5d085b5fe92dc4ffcd0e05d4145
SOK14 <yoshihiro_tsujii@yahoo.co.jp>
2022-12-30 17:28:33 +0900

    update

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"context"
+	"demo_goent/ent"
 	"log"
-
-	"entgo.io/ent/examples/fs/ent"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// -- Update
 func main() {
-
 	entOptions := []ent.Option{}
 	entOptions = append(entOptions, ent.Debug())
 	// client, err := ent.Open("mysql", "root:myrootpassword@tcp(localhost:3306)/mydb?parseTime=True", entOptions...)
@@ -19,35 +18,69 @@ func main() {
 		log.Fatalf("failed opening connection to mysql: %v", err)
 	}
 	defer client.Close()
-	ctx := context.Background()
-	if err := client.Schema.Create(ctx); err != nil {
-		log.Fatalf("failed creating schema changes: %v", err)
-	}
-	// cmp, err := client.Debug().Company.
-	// 	Create().
-	// 	SetName("companyA").
-	// 	Save(ctx)
-	// if err != nil {
-	// 	log.Fatalf("failed create company: %v", err)
-	// }
-	// log.Printf("cmp: %+v", cmp)
 
-	// usr, err := client.Debug().User.
-	// 	Create().
-	// 	SetFirstName("first name").
-	// 	SetLastName("last name").
-	// 	SetAge(20).
-	// 	SetEmail("example@example.co.jp").
-	// 	SetCompany(cmp).
-	// 	Save(ctx)
-	// if err != nil {
-	// 	log.Fatalf("failed create user: %v", err)
-	// }
-	// log.Printf("user: %+v", usr)
+	ctx := context.Background()
+	cmp, err := client.Debug().Company.
+		Create().
+		SetName("companyA").
+		Save(ctx)
+	if err != nil {
+		log.Fatalf("failed create company: %v", err)
+	}
+	log.Printf("cmp: %+v", cmp)
+
+	usr, err := client.Debug().User.
+		Create().
+		SetFirstName("first name").
+		SetLastName("last name").
+		SetAge(20).
+		SetEmail("example@example.co.jp").
+		SetCompany(cmp).
+		Save(ctx)
+	if err != nil {
+		log.Fatalf("failed create user: %v", err)
+	}
+	log.Printf("user: %+v", usr)
 	log.Print("ent sample done")
 }
 
-// -- スキーマ定義の確認
+// -- STEP4: Create
+// func main() {
+// 	entOptions := []ent.Option{}
+// 	entOptions = append(entOptions, ent.Debug())
+// 	// client, err := ent.Open("mysql", "root:myrootpassword@tcp(localhost:3306)/mydb?parseTime=True", entOptions...)
+// 	client, err := ent.Open("mysql", "root:myrootpassword@tcp(localhost:3306)/mydb?parseTime=True")
+// 	if err != nil {
+// 		log.Fatalf("failed opening connection to mysql: %v", err)
+// 	}
+// 	defer client.Close()
+
+// 	ctx := context.Background()
+// 	cmp, err := client.Debug().Company.
+// 		Create().
+// 		SetName("companyA").
+// 		Save(ctx)
+// 	if err != nil {
+// 		log.Fatalf("failed create company: %v", err)
+// 	}
+// 	log.Printf("cmp: %+v", cmp)
+
+// 	usr, err := client.Debug().User.
+// 		Create().
+// 		SetFirstName("first name").
+// 		SetLastName("last name").
+// 		SetAge(20).
+// 		SetEmail("example@example.co.jp").
+// 		SetCompany(cmp).
+// 		Save(ctx)
+// 	if err != nil {
+// 		log.Fatalf("failed create user: %v", err)
+// 	}
+// 	log.Printf("user: %+v", usr)
+// 	log.Print("ent sample done")
+// }
+
+// -- STEP3: テーブル定義反映
 // func main() {
 // 	client, err := ent.Open("mysql", "root:myrootpassword@tcp(localhost:3306)/mydb?parseTime=True")
 // 	if err != nil {
@@ -55,17 +88,28 @@ func main() {
 // 	}
 // 	defer client.Close()
 // 	ctx := context.Background()
-// if err := client.Schema.Create(ctx, migrate.WithForeignKeys(false)); err != nil {
-// 	log.Fatalf("failed printing schema changes: %v", err)
-// }
-// 	// dump migration changes to stdout
-// if err := client.Schema.WriteTo(ctx, os.Stdout, migrate.WithForeignKeys(false)); err != nil {
-// 	log.Fatalf("failed creating schema resources: %v", err)
-// }
+// 	if err := client.Schema.Create(ctx, migrate.WithForeignKeys(false)); err != nil {
+// 		log.Fatalf("failed printing schema changes: %v", err)
+// 	}
 // 	log.Print("ent sample done")
 // }
 
-// -- DB接続確認
+// -- STEP2: スキーマ定義の確認
+// func main() {
+// 	client, err := ent.Open("mysql", "root:myrootpassword@tcp(localhost:3306)/mydb?parseTime=True")
+// 	if err != nil {
+// 		log.Fatalf("failed opening connection to mysql: %v", err)
+// 	}
+// 	defer client.Close()
+// 	ctx := context.Background()
+// 	// dump migration changes to stdout
+// 	if err := client.Schema.WriteTo(ctx, os.Stdout, migrate.WithForeignKeys(false)); err != nil {
+// 		log.Fatalf("failed creating schema resources: %v", err)
+// 	}
+// 	log.Print("ent sample done")
+// }
+
+// -- STEP1: DB接続確認
 // func main() {
 // 	client, err := ent.Open("mysql", "root:myrootpassword@tcp(localhost:3306)/mydb?parseTime=True")
 // 	if err != nil {
